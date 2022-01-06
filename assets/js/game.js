b86b1a9f9bc0187475be76f047756352bd792eec
H0RSESH0E <94808313+H0RSESH0E@users.noreply.github.com>
2022-01-06 14:05:33 -0500

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,7 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
@@ -9,19 +10,19 @@ var enemyName = "Roborto";
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+window.alert("Welcome to Robot Gladiators! " + playerName);
+
 var fight = function() {
-    // Alert players that they are  starting the round
-    window.alert("Welcome to Robot Gladiators!");
 
-    // Subtract the value of 'playerAttack' from the value of enemyHealth' and use that result to update the value in the 'enemyHealth' variable
 
-    enemyHealth = enemyHealth - playerAttack
 
-    // log a resulting message to the console so we know that it worked
+var promtFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
-   
-    
-// put new code under this
+console.log(promtFight);
+
+if (promtFight === "FIGHT" || promtFight === "fight" || promtFight === "Fight") {
+enemyHealth = enemyHealth - playerAttack
+
 console.log(
     playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
   );
@@ -52,6 +53,20 @@ if (playerHealth <= 0) {
     // log a resulting message to the console so we know that it worked.
 
     console.log("Updated Player Health:" + playerHealth)
+}
+else if (promtFight === "SKIP" || promtFight === "skip" || promtFight === "Skip") {
+    var confirmSkip = window.confirm("Are you a coward?  It will cost you $2 to skip.  Click 'OK' to confirm or 'Cancel' to choose again.");
+    if (confirmSkip) {
+        window.alert(playerName + "is a coward and is skipping this fight. It's going to cost you $2!");
+        playerMoney = playerMoney - 2;
+    }
+    else {
+        fight();
+    }
+}
+else {
+    window.alert("You are the only one who can choose!")
+}
 };
 
 fight();
